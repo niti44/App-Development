b08edee1db1b3d00bc862562041afc4b1f923e94
niti44 <hce073bct025@hcoe.edu.np>
2022-01-17 11:32:32 +0545

    added new java files

--- src/main/java/com/example/AppDevelopment/Entity/dev1.java
+++ src/main/java/com/example/AppDevelopment/Entity/dev1.java
@@ -2,6 +2,8 @@ package com.example.AppDevelopment.Entity;
 
 public class dev1 {
     public static void main(String[] args) {
+        System.out.println("Hello Spring boot!!");
         System.out.println("hello world");
+        System.out.println("Hello DEVS!!");
     }
 }
